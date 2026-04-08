e60c2e80eca496d70b6981ec68ab41d6c5728fd2
xjuuhhh <juliasantoscursos05@gmail.com>
2026-04-08 11:20:56 -0300

    Comecei o projeto

---
+++ src/main/java/br/com/juliac/FastFuriousAPI/FastFuriousApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.juliac.FastFuriousAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFuriousApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFuriousApiApplication.class, args);
+	}
+
+}
